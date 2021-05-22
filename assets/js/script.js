7fe211528cf98fe6488603320f3f0303026a45cf
Tai Mujarin <tai.mujarin@gmail.com>
2021-05-21 18:49:52 -0700

    added start and end of quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,12 +80,63 @@ var answerEl = document.querySelector('.answers');
 
 
 
-// Function //
+// Functions //
 function resetTimer() {
     timer = 90;
 }
 
-// function home() {
-    questionEl.textContent = "Coding Quiz Challenge";
-    choicesEl.textContent = "Try to answer the following questions. You have 90 Seconds. Incorrect answers will deduct your score and time by 10.";
-//   }
+function startTimer() {
+    var startTimer = setInterval(() => {
+        if(timer > 0) {
+            timerEl.textContent = "Time: " + timer;
+            timer--;
+            
+        } 
+    }, 1000);
+}
+
+startTimer();
+
+function home() {
+    score = 0;
+    questionEl.innerHTML = "<h1><center>Coding Quiz Challenge</center></h1>";
+    choicesEl.innerHTML =
+      "<h2><center>Try to answer the following questions. You have 90 Seconds. Incorrect answers will deduct your score and time by 10.</center></h2>" +
+      "<br><center><button class='start-quiz' type='button'>Start Quiz</button></center>";
+}
+
+console.log(home);
+
+// Function when start quiz button is clicked //
+function startQuiz() {
+    startTimer();
+    if (questionCounter === 0) {
+      choicesEl.innerHTML = "";
+    } else if (questionCounter === 1) {
+      choicesEl.innerHTML = "";
+    } else if (questionCounter === 2) {
+      choicesEl.innerHTML = "";
+    } else if (questionCounter === 3) {
+      choicesEl.innerHTML = "";
+    } else if (questionCounter === 4) {
+      choicesEl.innerHTML = "";
+    } else if (questionCounter === 5) {
+      choicesEl.innerHTML = "";
+    } else {
+      endQuiz();
+    }
+}
+
+function endQuiz() {
+    questionCounter = 0;
+    timer = 0;
+    if (score < 0) {
+      score = 0;
+    }
+    scoreSubmit.addEventListener("click", clickedScore);
+}
+
+// Function when the submit high score button is clicked //
+function clickedScore() {
+    
+}
